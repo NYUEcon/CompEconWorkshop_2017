0516ad35e1883c38a0abaaba484c42df536dfc8a
Chase <cc7768@gmail.com>
2017-06-22 02:28:10 -0400

    Add code for lecture 1 images.

--- Lecture1/figure_generator.py
+++ Lecture1/figure_generator.py
@@ -67,6 +67,8 @@ fig.savefig("./images/smolyakrandomtensor.pgf")
 #
 def bisection(f, a, b, ftol=1e-4, xtol=1e-5):
     """
+    Uses bisection method to find zeros of f
+
     Parameters
     ----------
     f : Function
@@ -80,6 +82,7 @@ def bisection(f, a, b, ftol=1e-4, xtol=1e-5):
 
     # Iterate until done
     dist = 10.0
+    xtrace = []  # Keep track of each c
     while dist > xtol:
         # Compute midpoint
         c = (a + b)/2
@@ -93,10 +96,90 @@ def bisection(f, a, b, ftol=1e-4, xtol=1e-5):
             fa = fc
 
         dist = abs(b - a)
+        xtrace.append(c)
 
     # Check whether fc is close to zero
     warn("f(c) is not close to zero") if abs(fc) > ftol else None
 
-    return c, fc
+    return c, fc, xtrace
 
+
+def newton(f, df, x0, ftol=1e-4, xtol=1e-5):
+    """
+    Uses Newton's method to find zeros of f
+
+    Parameters
+    ----------
+    f : Function
+        Function that we want to find the zero of
+    df : Function
+        Derivative of function that we want to find the zero of
+    x0 : scalar(Float64)
+        Initial guess of the zero
+    """
+    # Initialize variables
+    xk = x0
+    fk = f(x0)
+    dfk = df(x0)
+
+    # Iterate till done
+    dist = 10.0
+    xtrace = []  # Keep track of each xk
+    while dist > xtol:
+        # Compute x_{k+1}
+        xkp1 = xk - fk/dfk
+        dist = abs(xkp1 - xk)
+        xtrace.append(xkp1)
+
+        # Update all values
+        xk = xkp1
+        fk = f(xkp1)
+        dfk = df(xkp1)
+
+    # Check whether fc is close to zero
+    warn("f(x_k) is not close to zero") if abs(fk) > ftol else None
+
+    return xk, fk, xtrace
+
+
+def secant(f, x0, xm1, ftol=1e-4, xtol=1e-5):
+    """
+    Uses secant method to find zeros of f
+
+    Parameters
+    ----------
+    f : Function
+        Function that we want to find the zero of
+    x0, xm1 : scalar(Float64)
+        Initial guesses of the zero
+    """
+    xk, xkm1 = x0, xm1
+    fk, fkm1 = f(xk), f(xkm1)
+
+    # Iterate till done
+    dist = 10.0
+    xtrace = []  # Keep track of each xk
+    while dist > xtol:
+        # Compute x_{k+1}
+        xkp1 = xk - fk*(xk - xkm1)/(fk - fkm1)
+        dist = abs(xkp1 - xk)
+        xtrace.append(xkp1)
+
+        # Update all values
+        xkm1, fkm1 = xk, fk
+        xk = xkp1
+        fk = f(xkp1)
+
+    # Check whether fc is close to zero
+    warn("f(x_k) is not close to zero") if abs(fk) > ftol else None
+
+    return xk, fk, xtrace
+
+
+f = lambda x: np.sin(x)
+df = lambda x: np.cos(x)
+
+xb, fb, xtraceb = bisection(f, -0.1, 0.5)
+xn, fn, xtracen = newton(f, df, -0.1)
+xs, fs, xtraces = secant(f, -0.1, -0.09)
 
